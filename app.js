4eaac52f5433b1971bfc43ec2aecc638b31df12e
John Woods <john.woods@gcsourcing.com>
2020-09-05 20:36:09 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var friends = [Menza, Thomas, Jordan, Cam, Dave];
+
+console.log(
+    // 0-99
+    // if num > 1, lines
+    // if num === 1, line
+    // do it for array items 0-4
+)
+
+// 99 lines of code in the file, 99 lines of code; John strikes one out, clears it all out, 98 lines of code in the file
